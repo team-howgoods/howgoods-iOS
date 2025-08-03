a1868b99c52b7ea13d7cad0be031232c4ef51dcd
_GGDol <bohg6799@naver.com>
2025-08-04 02:03:39 +0900

    fix: URL 인식 오류 수정
    - // 가 주석문 처리가 되어 URL 인식을 못해 Bundle Extension에서 http:// 처리
    
    Github issue #3

--- howgoods/howgoods/Common/Utils/Extensions/Bundle+.swift
+++ howgoods/howgoods/Common/Utils/Extensions/Bundle+.swift
@@ -12,7 +12,7 @@ extension Bundle {
     /// Info.plist에서 BASE_API_URL을 읽어오는 컴퓨티드 프로퍼티
     var baseAPIURL: URL {
         guard let urlString = object(forInfoDictionaryKey: "BASE_API_URL") as? String,
-              let url = URL(string: urlString) else {
+              let url = URL(string: "https://\(urlString)") else {
             fatalError("BASE_API_URL이 Info.plist에 설정되어 있지 않거나 잘못된 형식입니다.")
         }
         return url
