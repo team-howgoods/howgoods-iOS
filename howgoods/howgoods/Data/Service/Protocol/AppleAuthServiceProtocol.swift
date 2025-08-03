31b8d11eeaf4273c93770875d7c6f8801e8dbce0
_GGDol <bohg6799@naver.com>
2025-08-03 22:55:24 +0900

    feat: 인증 네트워크 요청을 추상화하는 프로토콜 추가
    Github issue #1

---
+++ howgoods/howgoods/Data/Service/Protocol/AppleAuthServiceProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  AppleAuthServiceProtocol.swift
+//  howgoods
+//
+//  Created by 양원식 on 8/3/25.
+//
+
+import RxSwift
+
+/// 인증 관련 네트워크 요청을 담당하는 서비스 계층
+protocol AuthNetworkServiceProtocol {
+    func loginWithApple(code: String) -> Observable<Result<AuthToken, Error>>
+    // TODO: loginWithNaver(token: String), loginWithKakao(token: String)
+}
