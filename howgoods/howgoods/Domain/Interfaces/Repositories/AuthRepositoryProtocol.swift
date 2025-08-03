c19c645cfca65aa6532f7d915f2a068c27354970
_GGDol <bohg6799@naver.com>
2025-08-03 22:59:33 +0900

    feat: Apple 로그인 기능을 위한 AuthRepository 프로토콜 정의
    Github issue #1

---
+++ howgoods/howgoods/Domain/Interfaces/Repositories/AuthRepositoryProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  AuthRepositoryProtocol.swift
+//  howgoods
+//
+//  Created by 양원식 on 8/3/25.
+//
+
+import RxSwift
+
+protocol AuthRepositoryProtocol {
+    func loginWithApple() -> Observable<Result<String, Error>>
+    func sendCodeToServer(code: String) -> Observable<Result<String, Error>>
+}
