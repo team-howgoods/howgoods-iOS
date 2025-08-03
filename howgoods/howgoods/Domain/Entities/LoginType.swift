068eb4947e66f65cc29eddde85e8b57a066b6e98
_GGDol <bohg6799@naver.com>
2025-08-03 23:00:23 +0900

    feat: 소셜 로그인 타입 구분을 위한 LoginType enum 추가
    Github issue #1

---
+++ howgoods/howgoods/Domain/Entities/LoginType.swift
@@ -0,0 +1,15 @@
+//
+//  LoginType.swift
+//  howgoods
+//
+//  Created by 양원식 on 8/3/25.
+//
+
+/// 로그인 타입을 구분하는 열거형
+enum LoginType {
+    case apple
+    case naver
+    case kakao
+    
+    // TODO: 추후 naver, Kakao 로그인 추가 시 분기 처리에 사용
+}
