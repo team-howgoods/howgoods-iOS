514d6e566ca552e48ae8717ea550d6ed7d471768
_GGDol <bohg6799@naver.com>
2025-08-06 20:33:34 +0900

    comment: LoginType 주석문 추가

--- howgoods/howgoods/Domain/Entities/LoginType.swift
+++ howgoods/howgoods/Domain/Entities/LoginType.swift
@@ -5,11 +5,18 @@
 //  Created by 양원식 on 8/3/25.
 //
 
-/// 로그인 타입을 구분하는 열거형
+/// 로그인 방식을 구분하는 열거형입니다.
+///
+/// Apple, Naver, Kakao 중 어떤 로그인 방식이 선택되었는지를 나타냅니다.
+/// 인증 요청 시 로그인 흐름을 분기 처리하는 데 사용됩니다.
 enum LoginType {
-    case apple
-    case naver
-    case kakao
     
-    // TODO: 추후 naver, Kakao 로그인 추가 시 분기 처리에 사용
+    /// Apple 로그인
+    case apple
+    
+    /// Naver 로그인
+    case naver
+    
+    /// Kakao 로그인
+    case kakao
 }
