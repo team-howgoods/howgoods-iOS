137549f18e14e208c6c6589c65acedd42d95a5c8
_GGDol <bohg6799@naver.com>
2025-08-14 02:23:27 +0900

    feat: SceneDelegate 코디네이터 적용
    Github issue #15

--- howgoods/howgoods/App/SceneDelegate.swift
+++ howgoods/howgoods/App/SceneDelegate.swift
@@ -6,21 +6,43 @@
 //
 
 import UIKit
+import NidThirdPartyLogin
+import KakaoSDKAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        let viewController = LoginViewController()
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
+
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+
+        let appCoordinator = AppCoordinator(window: window)
+        self.appCoordinator = appCoordinator
+        appCoordinator.start()
     }
 
+
+    // URL Scheme을 통해 앱으로 돌아왔을 때 호출됨
+    // TODO: 언제 쓰는지 몰라서 찾아봐야함...
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else { return }
+
+        if NidOAuth.shared.handleURL(url) {
+            // 네이버 로그인 URL 처리 성공
+        } else {
+            // 다른 URL 처리 필요 시 여기에 추가
+        }
+        
+        if AuthApi.isKakaoTalkLoginUrl(url) {
+            _ = AuthController.handleOpenUrl(url: url)
+        }
+    }
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
